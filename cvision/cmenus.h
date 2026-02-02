676feb88d1307ea690072d553db6bb0dba360aed
Jason Lee <jason@steeplesoft.com>
2026-02-02 16:35:53 -0600

    Code cleanup

--- cvision/cmenus.h
+++ cvision/cmenus.h
@@ -10,24 +10,27 @@ extern "C" {
 #endif
 
     /* Menu item helper functions */
-    tv_MenuItem *tv_menuitem_create(const char *name, tv_ushort command, tv_ushort key_code, tv_ushort *help_ctx, const char *p, tv_MenuItem *next);
-    tv_SubMenu* tv_submenu_create(const char* name, tv_ushort key_code);
-    tv_SubMenu* tv_submenu_add_next(tv_SubMenu* submenu, tv_SubMenu* next);
-    tv_SubMenu* tv_submenu_add_menuitem(tv_SubMenu* submenu, tv_MenuItem* item);
+    tv_MenuItem *tv_menuitem_create(const char *name, tv_ushort command, tv_ushort key_code, tv_ushort *help_ctx,
+                                    const char *p, tv_MenuItem *next);
+
+    tv_SubMenu *tv_submenu_create(const char *name, tv_ushort key_code);
+
+    tv_SubMenu *tv_submenu_add_next(tv_SubMenu *submenu, tv_SubMenu *next);
+
+    tv_SubMenu *tv_submenu_add_menuitem(tv_SubMenu *submenu, tv_MenuItem *item);
 
     /* TMenuBar functions */
-    tv_MenuBar* tv_menubar_create(tv_Rect bounds, tv_SubMenu* menu);
-    void tv_menubar_destroy(tv_MenuBar* menubar);
+    tv_MenuBar *tv_menubar_create(tv_Rect bounds, tv_SubMenu *menu);
+    void tv_menubar_destroy(tv_MenuBar *menubar);
 
 
     /* Status item helper functions */
-    tv_StatusItem* tv_statusitem_create(const char* text, tv_ushort key_code, tv_ushort command,
-                                         tv_StatusItem* next);
-    void tv_statusitem_destroy(tv_StatusItem* item);
+    tv_StatusItem *tv_statusitem_create(const char *text, tv_ushort key_code, tv_ushort command, tv_StatusItem *next);
+    void tv_statusitem_destroy(tv_StatusItem *item);
 
     /* TStatusLine functions */
-    tv_StatusLine* tv_statusline_create(tv_Rect bounds, tv_StatusItem* items);
-    void tv_statusline_destroy(tv_StatusLine* statusline);
+    tv_StatusLine *tv_statusline_create(tv_Rect bounds, tv_StatusItem *items);
+    void tv_statusline_destroy(tv_StatusLine *statusline);
 #ifdef __cplusplus
 }
 #endif
